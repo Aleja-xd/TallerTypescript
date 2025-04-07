b7c8297703251a5140ab2e2b1f0d0441b71b0365
María Sanabria <ma.sanabrias@uniandes.edu.co>
2025-04-06 21:55:04 -0500

    Taller parte 2

--- main.ts
+++ main.ts
@@ -6,7 +6,14 @@ const tableBody = document.getElementById("series") as HTMLTableSectionElement;
 function renderSeriesTable(): void {
     series.forEach((serie) => {
         let row = document.createElement("tr");
-        row.innerHTML = `<td>${serie.id}</td><td>${serie.name}</td><td>${serie.channel}</td><td>${serie.seasons}</td>`;
+        row.innerHTML = `<td>${serie.id}</td> <td><a href="#" class="serie-link">${serie.name}</a><td>${serie.channel}</td><td>${serie.seasons}</td>`;
+        
+        const link = row.querySelector(".serie-link") as HTMLAnchorElement;
+        link.addEventListener("click", (event) => {
+            event.preventDefault(); // Para que no recargue la página
+            mostrarDetalle(serie);
+        });
+
         tableBody.appendChild(row);
     });
 }
@@ -24,3 +31,19 @@ function mostrarPromedio(): void {
 }
 
 mostrarPromedio();
+
+const detailContainer: HTMLElement = document.getElementById('serie-detail')!;
+
+function mostrarDetalle(serie: Serie): void {
+    //La imagen no me carga en live server, sin embargo en http://localhost:3000/ si
+    detailContainer.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <img src="${serie.image}" class="card-img-top" alt="${serie.name}">
+      <div class="card-body">
+        <h5 class="card-title">${serie.name}</h5>
+        <p class="card-text">${serie.description}</p>
+        <a href="${serie.url}" target="_blank">${serie.url}</a>
+      </div>
+    </div>
+  `;
+}
--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,12 @@ var tableBody = document.getElementById("series");
 function renderSeriesTable() {
     series.forEach(function (serie) {
         var row = document.createElement("tr");
-        row.innerHTML = "<td>".concat(serie.id, "</td><td>").concat(serie.name, "</td><td>").concat(serie.channel, "</td><td>").concat(serie.seasons, "</td>");
+        row.innerHTML = "<td>".concat(serie.id, "</td> <td><a href=\"#\" class=\"serie-link\">").concat(serie.name, "</a><td>").concat(serie.channel, "</td><td>").concat(serie.seasons, "</td>");
+        var link = row.querySelector(".serie-link");
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Para que no recargue la página
+            mostrarDetalle(serie);
+        });
         tableBody.appendChild(row);
     });
 }
@@ -17,3 +22,7 @@ function mostrarPromedio() {
     promedioElement.textContent = "El promedio de temporadas es: ".concat(Math.round(calcularPromedioTemporadas()));
 }
 mostrarPromedio();
+var detailContainer = document.getElementById('serie-detail');
+function mostrarDetalle(serie) {
+    detailContainer.innerHTML = "\n    <div class=\"card\" style=\"width: 18rem;\">\n      <img src=\"".concat(serie.image, "\" class=\"card-img-top\" alt=\"").concat(serie.name, "\">\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">").concat(serie.name, "</h5>\n        <p class=\"card-text\">").concat(serie.description, "</p>\n        <a href=\"").concat(serie.url, "\" target=\"_blank\">").concat(serie.url, "</a>\n      </div>\n    </div>\n  ");
+}
